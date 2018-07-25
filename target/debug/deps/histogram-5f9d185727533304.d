7883af897762dbc93e53daf35e243bcc85271b7b
Daniel Busaba <danielbusaba99@hotmail.com>
2018-07-25 11:04:24 -0400

    Added function for iterating through historam, demonstrated module use

--- target/debug/deps/histogram-5f9d185727533304.d
+++ target/debug/deps/histogram-5f9d185727533304.d
@@ -1,5 +1,6 @@
-/media/dbusaba/SharedData/University/Research/Rust/histogram/target/debug/deps/histogram-5f9d185727533304: src/main.rs
+/media/dbusaba/SharedData/University/Research/Rust/histogram/target/debug/deps/histogram-5f9d185727533304: src/main.rs src/histogram.rs
 
-/media/dbusaba/SharedData/University/Research/Rust/histogram/target/debug/deps/histogram-5f9d185727533304.d: src/main.rs
+/media/dbusaba/SharedData/University/Research/Rust/histogram/target/debug/deps/histogram-5f9d185727533304.d: src/main.rs src/histogram.rs
 
 src/main.rs:
+src/histogram.rs:
--- target/debug/histogram.d
+++ target/debug/histogram.d
@@ -1,1 +1,1 @@
-/media/dbusaba/SharedData/University/Research/Rust/histogram/target/debug/histogram: /media/dbusaba/SharedData/University/Research/Rust/histogram/src/main.rs
+/media/dbusaba/SharedData/University/Research/Rust/histogram/target/debug/histogram: /media/dbusaba/SharedData/University/Research/Rust/histogram/src/histogram.rs /media/dbusaba/SharedData/University/Research/Rust/histogram/src/main.rs
